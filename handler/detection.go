bc8796d7134926a46a1a43256712fcb7b21b8fea
filipecastro <filipeborato@hotmail.com>
2022-09-05 17:48:08 +0200

    feat: Flow for standard deviation

--- handler/detection.go
+++ handler/detection.go
@@ -1,1 +1,20 @@
 package handler
+
+import (
+	"anomaly-detection-go/repository"
+	"anomaly-detection-go/utils"
+	"fmt"
+	"strconv"
+)
+
+func AnomalyDetection() {
+	var values []float64
+	rootDir := utils.RootDir()
+	data := repository.ExtractCsv(rootDir+"/data/machine_temperature_system_failure.csv", ',')
+
+	for _, line := range data {
+		value, _ := strconv.ParseFloat(line.Value, 64)
+		values = append(values, value)
+	}
+	fmt.Println(values)
+}
