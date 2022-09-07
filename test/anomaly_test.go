1434d3f75348ac1b8d414f1fb5199dee1585bf89
Your Name <you@example.com>
2022-09-07 18:36:29 +0200

    feat: Include Tests

--- test/anomaly_test.go
+++ test/anomaly_test.go
@@ -1,8 +1,10 @@
 package test
 
 import (
+	"anomaly-detection-go/handler"
 	"anomaly-detection-go/repository"
 	"anomaly-detection-go/utils"
+	"math/rand"
 	"testing"
 )
 
@@ -22,4 +24,31 @@ func TestReaderCsv(t *testing.T) {
 	rootDir := utils.RootDir()
 	filePath := rootDir + "/data/machine_temperature_system_failure.csv"
 	repository.ExtractCsv(filePath, ',')
+}
+func testGenerateValues() []float64 {
+	values := make([]float64, 60)
+	for n := 0; n < 60; n++ {
+		values[n] = rand.Float64()
+	}
+	return values
+}
+func TestMean(t *testing.T) {
+	values := testGenerateValues()
+	utils.Mean(values)
+}
+
+func TestVariance(t *testing.T) {
+	values := testGenerateValues()
+	utils.Variance(values)
+}
+func TestStandardDesviation(t *testing.T) {
+	values := testGenerateValues()
+	utils.StandardDeviation(values)
+}
+func TestDeviation(t *testing.T) {
+	values := testGenerateValues()
+	utils.Deviation(values)
+}
+func TestAnomalyDetection(t *testing.T) {
+	handler.AnomalyDetection()
 }
