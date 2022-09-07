dc930476ad665155abda7d5a302c31a07febd3c5
Your Name <you@example.com>
2022-09-07 19:35:49 +0200

    feat: Refactoring model and returning case event struct

--- model/anomaly-detection.go
+++ model/anomaly-detection.go
@@ -1,28 +1,32 @@
 package model
 
 import (
+	"anomaly-detection-go/entity"
 	"anomaly-detection-go/utils"
-	"fmt"
 )
 
-func AnomalyDetection(values []float64) {
+func AnomalyDetection(values []float64, csv []entity.CSV) []entity.AnomalyDetection {
+	var anomalyDetection []entity.AnomalyDetection
+	var level string
 	mean := utils.Mean(values)
 	deviations := utils.Deviation(values)
 	standardDeveiation := utils.StandardDeviation(values)
 
-	for _, deviation := range deviations {
+	for i, deviation := range deviations {
 		if deviation/mean > 0.50 {
-			fmt.Println(mean)
-			fmt.Println(deviation)
-			fmt.Println(deviation / mean)
-			fmt.Println("Alert")
+			level = "Alert"
 		} else if standardDeveiation < deviation {
-			fmt.Println(mean)
-			fmt.Println(deviation)
-			fmt.Println(standardDeveiation)
-			fmt.Println("Warn")
+			level = "Warn"
 		} else {
-			fmt.Println("Info")
+			level = "Info"
 		}
+		anomaly := entity.AnomalyDetection{
+			Timestamp: csv[i].Timestamp,
+			Value:     csv[i].Value,
+			Level:     level,
+		}
+		anomalyDetection = append(anomalyDetection, anomaly)
+
 	}
+	return anomalyDetection
 }
