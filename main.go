f7a993bbf6c64cc270d9671cb7fd99ef91ebefcc
Your Name <you@example.com>
2022-09-08 12:38:23 +0200

    feat: Creating an API for the status JSON response

--- main.go
+++ main.go
@@ -1,7 +1,16 @@
 package main
 
-import "anomaly-detection-go/handler"
+import (
+	"anomaly-detection-go/handler"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
 
 func main() {
-	handler.AnomalyDetection()
+	router := mux.NewRouter().StrictSlash(true)
+	router.HandleFunc("/", handler.Alive)
+	router.HandleFunc("/anomaly-detection", handler.AnomalyDetection)
+	log.Fatal(http.ListenAndServe(":8080", router))
 }
